653ef3f76db81b97f36954854320421c77a6e16d
Vinh nheo <vinh.lamminh@hcmut.edu.vn>
2023-03-08 09:08:04 +0700

    update

---
+++ app/src/main/java/Demo/Android/MainActivity3.java
@@ -0,0 +1,117 @@
+package Demo.Android;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.github.angads25.toggle.interfaces.OnToggledListener;
+import com.github.angads25.toggle.model.ToggleableView;
+import com.github.angads25.toggle.widget.DayNightSwitch;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.nio.charset.Charset;
+
+public class MainActivity3 extends AppCompatActivity {
+    MQTTHelper mqttHelper;
+    TextView txtTemp,txtHumi,txtLight,tView;
+    SeekBar sBar;
+
+    DayNightSwitch btnLight ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        txtTemp = findViewById(R.id.Temperature);
+        txtHumi = findViewById(R.id.Humidity);
+        txtLight=findViewById(R.id.light);
+        btnLight =  findViewById(R.id.lightswitch);
+        tView = (TextView) findViewById(R.id.textview1);
+        sBar = (SeekBar) findViewById(R.id.seekBar1);
+        sBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int pval = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                pval = progress;
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //write custom code to on start progress
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                tView.setText(pval + "/" + seekBar.getMax());
+            }
+        });
+        btnLight.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn) {
+                if(isOn == true){
+                    sendDataMQTT("LamVinh/feeds/button1\n", "1");
+                }
+                else{
+                    sendDataMQTT("LamVinh/feeds/button1\n", "0");
+                }
+            }
+        });
+        startMQTT();
+    }
+    public void sendDataMQTT(String topic, String value){
+        MqttMessage msg = new MqttMessage();
+        msg.setId(1234);
+        msg.setQos(0);
+        msg.setRetained(false);
+        byte[] b = value.getBytes(Charset.forName("UTF-8"));
+        msg.setPayload(b);
+
+        try {
+            mqttHelper.mqttAndroidClient.publish(topic, msg);
+        }catch (MqttException e){
+        }
+    }
+    public void startMQTT(){
+        mqttHelper = new MQTTHelper(this    );
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d("TEST",topic + "---" + message.toString());
+                if(topic.contains("humi-info")){
+                    txtHumi.setText(message.toString()+"  %");
+                }
+                else if(topic.contains("temp-info")){
+                    txtTemp.setText(message.toString()+"  °C");
+                }
+                else if(topic.contains("light2")){
+                    txtLight.setText(message.toString()+"   lux");
+                }
+                else if(topic.contains("button1")){
+                    if(message.toString().equals("1")){
+                        btnLight.setOn(true);
+                    }
+                    else btnLight.setOn(false);
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+    }
+}
