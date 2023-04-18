fefc075b776447cd3a5dee64535940c529c02158
DanteBartel <dantedbartel@gmail.com>
2023-04-18 22:29:14 +0700

    CP 230418 10:29

--- app/src/main/java/Demo/Android/AppCompatActivityExtended.java
+++ app/src/main/java/Demo/Android/AppCompatActivityExtended.java
@@ -1,11 +1,8 @@
 package Demo.Android;
 
-//import android.support.v7.app.AppCompatActivity;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
-//import androidx.appcompat.app.AppCompatActivity;
 
 public class AppCompatActivityExtended extends AppCompatActivity {
     public void updateSensorValue(JSONObject jsonObject) {}
--- app/src/main/java/Demo/Android/MainActivity3.java
+++ app/src/main/java/Demo/Android/MainActivity3.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Toast;
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
